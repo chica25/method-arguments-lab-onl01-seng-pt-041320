a2c7da4a822c0164030a90cf23cbba943bc5bed5
Patricia Cordero <patricia@patriciaco.com>
2020-04-16 18:06:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,9 +5,26 @@ def introduction(name)
 end
   introduction("Josh")
   
-  
-#1  
+
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   introduction_with_language("Dan", "Ember.js")
+
+
+
+
+
+
+
+
+
+
+
+
+#1  
+# def introduction_with_language(name, language)
+#   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+# end
+#   introduction_with_language("Dan", "Ember.js")
